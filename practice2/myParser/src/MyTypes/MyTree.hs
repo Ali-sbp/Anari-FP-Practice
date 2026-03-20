6a9a55fd8d374c8259ebae6635facfb67ec25717
Ali-sbp <mohammad.anari3@gmail.com>
2026-03-20 08:26:16 +0300

    added second implementation for MyTree (each leaf applies to the whole tree)

--- practice2/myParser/src/MyTypes/MyTree.hs
+++ practice2/myParser/src/MyTypes/MyTree.hs
@@ -51,3 +51,38 @@ ghci> Leaf 1 <* Leaf 2
 Leaf 1
 -}
 
+---alternate application type : whole function Tree applied to each single leaf of values: 
+data MyTree1 a = Leaf1 a | Node1 a (MyTree1 a)(MyTree1 a) deriving (Show,Eq)
+instance Functor MyTree1 where 
+    fmap f (Leaf1 a) = Leaf1 (f a)
+    fmap f (Node1 x y z) = Node1 (f x) (fmap f y)(fmap f z) 
+tstffs3 = fmap (+1) (Leaf1 1)
+tstffs4= fmap (+2) (Node1 4 (Leaf1 1) (Node1 4 (Leaf1 1)(Leaf1 3)))
+instance Applicative MyTree1 where
+    pure x = Leaf1 x 
+    Leaf1 f <*> valueTree = fmap f valueTree
+    Node1 f leftFuncTree rightFuncTree <*> valueTree = 
+        Node1 (f (root valueTree))
+             (leftFuncTree <*> valueTree)
+             (rightFuncTree <*> valueTree)
+        where
+            root (Leaf1 value) = value
+            root (Node1 value _ _) = value
+
+-- case: Leaf1 func <*> valueTree  ==>  fmap func over the whole tree
+-- Leaf1 (*2) <*> Node1 3 (Leaf1 1) (Leaf1 4)  ==>  Node1 6 (Leaf1 2) (Leaf1 8)
+tst1_leaf_func = Leaf1 (*2) <*> Node1 3 (Leaf1 1) (Leaf1 4)
+
+-- case: Node1 func leftFuncTree rightFuncTree <*> valueTree
+-- each subtree of funcs broadcasts over the entire valueTree
+-- Node1 (+1) (Leaf1 (*2)) (Leaf1 (+10)) <*> Node1 5 (Leaf1 3) (Leaf1 7)
+--   root:         (+1) applied to root of valueTree (5)        ==>  6
+--   leftFuncTree: Leaf1 (*2) <*> entire valueTree              ==>  Node1 10 (Leaf1 6) (Leaf1 14)
+--   rightFuncTree: Leaf1 (+10) <*> entire valueTree            ==>  Node1 15 (Leaf1 13) (Leaf1 17)
+tst1_node_func = Node1 (+1) (Leaf1 (*2)) (Leaf1 (+10)) <*> Node1 5 (Leaf1 3) (Leaf1 7)
+
+-- Applicative law: pure id <*> t == t
+tst1_law_identity = (pure id <*> Node1 1 (Leaf1 2) (Leaf1 3)) == Node1 1 (Leaf1 2) (Leaf1 3)
+
+-- Applicative law: pure f <*> pure x == pure (f x)
+tst1_law_homomorphism = (pure (+1) <*> pure 5 :: MyTree1 Int) == pure (6 :: Int)
