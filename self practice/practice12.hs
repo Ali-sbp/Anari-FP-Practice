4d4ebb557a84cf195131083e34db5b0f9ca50139
Ali-sbp <mohammad.anari3@gmail.com>
2026-03-26 23:28:22 +0300

    added self practice files, over wrote something in practice12.hs, dunno what it was, too tired to check, god bless lol

--- self practice/practice12.hs
+++ self practice/practice12.hs
@@ -1,38 +1,90 @@
---practiced for MyAttoparsec.hs
-import Data.Attoparsec.Text (Parser, parseOnly, digit, char, many1)
---import Data.Text.Internal
-import Control.Applicative ((<|>))
-import Data.Char(digitToInt)
-import Data.Text (pack)  -- to convert String -> Text
+import Data.Foldable as FO
+import Data.Monoid as M
+import GHC.RTS.Flags (MiscFlags(installSEHHandlers))
 
+data Drirection = North | South | East | West 
 
+instance Show Drirection where 
+    show North = "N"
+    show South = "S"
+    show East = "E"
+    show West = "W"
 
+opposite :: Drirection -> Drirection
+opposite North = South
+opposite South = North
+opposite West = East
+opposite East = West
 
+instance Eq Drirection where 
+    North == North = True
+    South == South = True 
+    East == East = True 
+    West == West = True 
+    _ == _ = False
+    --x == y = show x == show y 
 
-digitsAtto :: Parser Int 
-digitsAtto = foldl(\x d -> x*10 + d) 0 <$> many1 (digitToInt <$> digit)
+instance Enum Drirection where 
+    toEnum 0 = North
+    toEnum 1 = South
+    toEnum 2 = East
+    toEnum 3 = West
+    fromEnum North = 0 
+    fromEnum South = 1
+    fromEnum East = 2 
+    fromEnum West = 3
 
-multAtto :: Parser Int 
-multAtto = (*) <$> digitsAtto <* char '*' <*> digitsAtto
+instance Bounded Drirection where 
+    minBound = North
+    maxBound = West
+allDirs :: [Drirection]
+allDirs = [minBound .. maxBound]
 
-plusAtto :: Parser Int 
-plusAtto = (+) <$> digitsAtto <* char '+' <*> digitsAtto
+data MyEither e a = MyLeft e | MyRight a deriving (Show,Eq)
 
-plusOrMultAttoparsec :: Parser Int 
-plusOrMultAttoparsec = plusAtto <|> multAtto
+instance Foldable (MyEither e) where 
+    foldr _ x (MyLeft y) = x
+    foldr f z (MyRight x) = f x z
+    --foldMap _ (MyLeft y) = mempty 
+    --foldMap f (MyRight x) = f x
 
-tst1= parseOnly plusOrMultAttoparsec (pack "12*345")
+tst1 = getSum 
 
---
-{-
-helper to keep integrity of main
-note how I pack the string for parseOnly, am I smart? lol 
--}
-runParser :: Parser Int -> String -> Either String Int 
-runParser p input = parseOnly p (pack input) 
-{-
-ghci> :t parseOnly 
-parseOnly :: Parser a -> Data.Text.Internal.Text -> Either String a
-just like parsec just returns the result and throws away the rest
--}
-tst2 = runParser plusOrMultAttoparsec "12*345asd"
+toList' :: MyEither e a -> [a]
+toList' = foldr (:) [] 
+
+tst2 = toList' (MyLeft "err")
+
+data MyMaybe a = MyJust a | MyNothing deriving (Show,Eq)
+
+instance Foldable MyMaybe where 
+    foldr _ x MyNothing = x
+    foldr f x (MyJust y) = f y x 
+
+instance Semigroup a=> Semigroup (MyMaybe a) where    
+    x <> MyNothing = x 
+    MyNothing <> x = x
+    (MyJust x) <> (MyJust y) = MyJust (x <> y)
+
+instance Semigroup a => Monoid (MyMaybe a) where 
+    mempty = MyNothing  
+
+instance Functor MyMaybe where 
+    fmap _ MyNothing = MyNothing 
+    fmap f (MyJust x) = MyJust (f x)
+
+instance Applicative MyMaybe where 
+    pure x = MyJust x 
+    MyNothing <*> _ = MyNothing 
+    _ <*> MyNothing = MyNothing 
+    (MyJust f) <*> (MyJust  y) = MyJust (f y)
+    
+myLiftA2' :: (a -> b -> c) -> MyMaybe a -> MyMaybe b -> MyMaybe c 
+myLiftA2' f ma mb = (f <$> ma) <*> mb 
+
+tst3 = myLiftA2' (+) (MyJust 2)(MyJust 3)
+
+data MyTree a = Leaf a | Node a (MyTree a)(MyTree a) deriving (Show,Eq)
+
+instance Foldable MyTree where 
+   -- foldMap f 
---
+++ self practice/practice13.hs
@@ -0,0 +1,108 @@
+module MyParsers.MyParser (runParser,charA, satisfy, char, lower, upper, digit, multiplication, lowers, digits, finalMult,finalPlus,plusOrMult) where
+
+import MyMaybe (MyMaybe(..))
+
+
+import Data.Char(isLower,isUpper,digitToInt,isDigit)
+import Control.Applicative(Alternative(..))
+
+--data MyMaybe a = MyJust a | MyNothing deriving (Show,Eq)
+
+newtype Parser tok a = Parser {runParser :: [tok] -> MyMaybe ([tok], a)}
+
+charA :: Parser Char Char 
+charA = Parser f where 
+    f (x : xs ) | x =='A' = MyJust(xs , x)
+    f               _ = MyNothing
+
+satisfy :: (tok -> Bool) -> Parser tok tok 
+satisfy p = Parser f where 
+    f ( x : xs )  | p x = MyJust(xs , x)
+    f               _   = MyNothing
+
+
+char :: Char -> Parser Char Char 
+char c = satisfy (==c)
+
+lower :: Parser Char Char 
+lower = satisfy isLower
+
+upper :: Parser Char Char 
+upper = satisfy isUpper
+
+digit :: Parser Char Int 
+digit = digitToInt <$> satisfy isDigit
+
+instance Functor (Parser tok) where 
+   -- fmap :: (a->b) -> Parser tok a -> Parser tok b 
+    fmap g (Parser u) = Parser f where 
+        f xs = case u xs of 
+            MyNothing -> MyNothing
+            MyJust(xs', x) -> MyJust(xs', g x)
+
+instance Applicative (Parser tok) where 
+   -- pure :: a -> Parser tok a 
+    pure x = Parser (\asd -> MyJust (asd, x))
+
+   -- (<*>) :: Parser tok a -> Parser tok a -> Parser tok a 
+    Parser u <*> Parser v = Parser f where 
+        f xs = case u xs of 
+            MyNothing -> MyNothing 
+            MyJust(xs', g) -> case v xs' of 
+                MyNothing -> MyNothing
+                MyJust (xs'', x) -> MyJust(xs'', g x) 
+
+instance Alternative (Parser tok) where 
+   -- empty :: Parser tok a 
+    empty = Parser (\_ -> MyNothing)
+
+   -- (<|>) :: Parser tok a -> Parser tok a -> Parser tok a 
+    Parser u <|> Parser v = Parser f where 
+        f xs = case u xs of 
+            MyNothing -> v xs
+            result -> result 
+
+multiplication :: Parser Char Int 
+multiplication = (*) <$> digit <* char '*' <*> digit
+
+lowers :: Parser Char String 
+lowers = ((:) <$> lower <*> lowers) <|> pure [] -- or pure "". same thing in this context
+
+digits :: Parser Char Int 
+digits = foldl(\x d -> x*10 + d) 0 <$> some digit
+
+
+finalMult :: Parser Char Int 
+finalMult = (*) <$> digits <* char '*' <*> digits 
+
+finalPlus :: Parser Char Int 
+finalPlus = (+) <$> digits <* char '+' <*> digits 
+
+plusOrMult :: Parser Char Int 
+plusOrMult = finalMult <|> finalPlus 
+
+
+digitsNeg :: Parser Char Int 
+digitsNeg = (negate <$> (char '-' *> digits))  <|> ( digits) 
+
+tstNeg = fmap (*2) <$> runParser digitsNeg "-123asd"
+
+data Op = Add | Sub | Mul | Div 
+operator :: Parser Char Op 
+operator = Parser f where 
+    f (x : xs)  |x == '+' = MyJust (xs, Add)
+                |x == '-' = MyJust (xs, Sub)
+                |x == '*' = MyJust (xs, Mul)
+                |x == '/' = MyJust (xs, Div) 
+    f           _ = MyNothing 
+
+calculate :: Int -> Op  -> Int -> Int 
+calculate  x Add  y = x + y
+calculate  x Sub  y = x - y 
+calculate  x Div  y = x `div` y 
+calculate  x Mul   y = x * y 
+
+expr :: Parser Char Int 
+expr = calculate <$> digits <*> operator <*> digits
+
+tstexpr=runParser expr "12-4abc"
